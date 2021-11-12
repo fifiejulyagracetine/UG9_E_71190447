4d756c7854b4f79bf3911a06d60ddf48e566df19
Fifie July Agracetine <fifie.july@ti.ukdw.ac.id>
2021-11-12 08:55:14 +0700

    push nomor 3

---
+++ 3_E_71190447.py
@@ -0,0 +1,7 @@
+belanja_1 = str(input("Masukkan daftar belanja 1:"))
+belanja_2 = str(input("Masukkan daftar belanja 2:"))
+tambahan_1 = str(input("Tambahkan data ke daftar belanja 1:"))
+tambahan_2 = str(input("Tambahkan data ke daftar belanja 2:"))
+
+print("Daftar belanja 1 adalah",belanja_1,",",tambahan_1)
+print("Daftar belanja 2 adalah",belanja_2, ",",tambahan_2)
