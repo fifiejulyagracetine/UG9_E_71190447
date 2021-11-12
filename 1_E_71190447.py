bed84432c38cb54df4b188855fecc860b162ae06
Fifie July Agracetine <fifie.july@ti.ukdw.ac.id>
2021-11-12 08:51:14 +0700

    push nomor 1

---
+++ 1_E_71190447.py
@@ -0,0 +1,6 @@
+alas_atas = int(input("Masukkan alas atas : "))
+alas_bawah = int(input("Masukkan alas bawah :"))
+tinggi = int(input("Masukkan tinggi :"))
+rumus_luas_trapesium = 1/2*(alas_atas+alas_bawah)*tinggi
+
+print ("Luas trapesium adalah :", rumus_luas_trapesium)
